78888d0f3efccc0d6e2ede6498d5fe28b0adcfa0
anaa-14 <riosanac1884@gmail.com>
2025-02-15 11:25:20 -0800

    Initial Commit

---
+++ YouAreAwsome/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  YouAreAwsome
+//
+//  Created by Student2 on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = " "
+    @State private var imageString = " "
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.pink)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            Spacer()
+            
+            Button("Press Me!") {
+                let message1 = "You are Awsome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                //
+                //                if message == message1 {
+                //                    message = message2
+                //                    imageString = imageString1
+                //                } else {
+                //                    message = message1
+                //                    imageString = imageString2
+                //                }
+                //            }
+                
+                message = (message == message1 ? message2: message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.pink)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwsomeTests/YouAreAwsomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeTests.swift
+//  YouAreAwsomeTests
+//
+//  Created by Student2 on 1/27/25.
+//
+
+import Testing
+@testable import YouAreAwsome
+
+struct YouAreAwsomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
