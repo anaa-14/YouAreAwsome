d3cc041d9ad5cfcc38d86bf260a43e67c3fa80df
anaa-14 <riosanac1884@gmail.com>
2025-02-21 12:23:26 -0800

    adding all 10 images

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,42 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = " "
-    @State private var imageString = " "
+    @State private var imageName = " "
+    @State private var imageNumber = 0
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.pink)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You are Awsome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                //
-                //                if message == message1 {
-                //                    message = message2
-                //                    imageString = imageString1
-                //                } else {
-                //                    message = message1
-                //                    imageString = imageString2
-                //                }
-                //            }
+                let message3 = "Fabulous? That's You!"
                 
-                message = (message == message1 ? message2: message1)
-                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+                if message == message1{
+                    message = message2
+                } else if message == message2{
+                    message = message3
+                } else {
+                    message = message1
+                }
+           
+//               message = (message == message1 ? message2: message1)
+//                imageName = (imageName == "image0" ? "image1" : "image0" )
+
+                imageName = "image\(imageNumber)"
+//                imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.pink)
         }
         .padding()
         
